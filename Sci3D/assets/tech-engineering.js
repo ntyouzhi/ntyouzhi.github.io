ec2c1926d91fb63c9b20a650e7ec42bb2e7ffa9b
ntyouzhi <ntyouzhi@gmail.com>
2026-09-15 21:53:54 +0800

    Add files via upload

--- Sci3D/assets/tech-engineering.js
+++ Sci3D/assets/tech-engineering.js
@@ -5,14 +5,14 @@
       title: "自行车结构探索",
       sub: "部件观察 · 系统演示 · 力与运动",
       icon: "车",
-      src: "assets/tech/bike.html?embed=1",
+      src: "assets/tech/bike.html?embed=1&v=20260915-2",
       name: "自行车结构探索"
     },
     {
       title: "联合收割机结构探索",
       sub: "五个系统 · 四步收获 · 协同工作",
       icon: "收",
-      src: "assets/tech/harvester.html?embed=1",
+      src: "assets/tech/harvester.html?embed=1&v=20260915-2",
       name: "联合收割机结构探索"
     }
   ];
